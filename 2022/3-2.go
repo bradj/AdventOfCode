e05db3f9f9fdfca26028bfea1129581a97f1e2cf
Brad Janke <bradj@users.noreply.github.com>
2022-12-04 07:19:57 -0600

    day 3 part 2

--- 2022/3-2.go
+++ 2022/3-2.go
@@ -1,5 +1,56 @@
 package twenttwo
 
-func D3p2() {
+import (
+	"fmt"
 
+	"github.com/bradj/AdventOfCode/util"
+)
+
+func convertRune(char rune) int {
+	code := int(char)
+	if code < 91 {
+		code -= 38 // uppercase conversion
+	} else {
+		code -= 96 // lowercase conversion
+	}
+
+	return code
+}
+
+func common(group []string) (rune, error) {
+	common := make(map[rune]map[int]bool)
+
+	for idx, line := range group {
+		for _, char := range line {
+			if common[char] == nil {
+				common[char] = make(map[int]bool)
+			}
+
+			common[char][idx] = true
+
+			if len(common[char]) == 3 {
+				return char, nil
+			}
+		}
+	}
+
+	return ' ', fmt.Errorf("not found")
+}
+
+func D3p2() {
+	groups := util.GetItems("2022/3.input")
+	total := 0
+	grpNum := 0
+
+	for grpNum < len(groups)-1 {
+		char, err := common(groups[grpNum : grpNum+3])
+		if err != nil {
+			panic(err)
+		}
+
+		grpNum += 3
+		total += convertRune(char)
+	}
+
+	fmt.Printf("%d\n", total)
 }
