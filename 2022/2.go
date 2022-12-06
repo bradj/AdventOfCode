53d074c58f1fa527571a90df14039add61f72d62
Brad Janke <bradj@users.noreply.github.com>
2022-12-06 17:07:29 -0600

    2022 cleanup

--- 2022/2.go
+++ 2022/2.go
@@ -64,3 +64,41 @@ func D2p1() {
 
 	fmt.Printf("total: %d\n", total)
 }
+
+var outcomes map[string]map[string]string
+
+func D2p2() {
+	items := util.GetItems("2022/2.input")
+
+	outcomes = make(map[string]map[string]string)
+
+	outcomes[OpRock] = make(map[string]string)
+	outcomes[OpRock]["X"] = Scissors // lose
+	outcomes[OpRock]["Y"] = Rock     // draw
+	outcomes[OpRock]["Z"] = Paper    // win
+
+	outcomes[OpPaper] = make(map[string]string)
+	outcomes[OpPaper]["X"] = Rock
+	outcomes[OpPaper]["Y"] = Paper
+	outcomes[OpPaper]["Z"] = Scissors
+
+	outcomes[OpScissors] = make(map[string]string)
+	outcomes[OpScissors]["X"] = Paper
+	outcomes[OpScissors]["Y"] = Scissors
+	outcomes[OpScissors]["Z"] = Rock
+
+	total := 0
+
+	for _, item := range items {
+		if item == "" {
+			continue
+		}
+
+		round := strings.Split(item, " ")
+		myChoice := outcomes[round[0]][round[1]]
+		result := choices[round[0]][myChoice] + choiceScore[myChoice]
+		total += result
+	}
+
+	fmt.Printf("total: %d\n", total)
+}
--- 2022/4.go
+++ 2022/4.go
@@ -7,6 +7,38 @@ import (
 	"github.com/bradj/AdventOfCode/util"
 )
 
+func contains(start1 int, end1 int, start2 int, end2 int) bool {
+	if start1 >= start2 && end1 <= end2 {
+		return true
+	}
+
+	if start2 >= start1 && end2 <= end1 {
+		return true
+	}
+
+	return false
+}
+
+func D4p1(items []string) {
+	total := 0
+
+	for _, line := range items {
+		if line == "" {
+			continue
+		}
+
+		pairs := strings.Split(line, ",")
+		p1 := strings.Split(pairs[0], "-")
+		p2 := strings.Split(pairs[1], "-")
+
+		if contains(util.Intme(p1[0]), util.Intme(p1[1]), util.Intme(p2[0]), util.Intme(p2[1])) {
+			total++
+		}
+	}
+
+	fmt.Printf("%d\n", total)
+}
+
 func p2contains(start1 int, end1 int, start2 int, end2 int) bool {
 	if start1 <= end2 && start1 >= start2 {
 		return true
